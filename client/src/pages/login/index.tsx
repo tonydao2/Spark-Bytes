b864f15d80af101d13e212f9fc113667138039ae
Tony Dao <tonydao@crc-dot1x-nat-10-239-89-157.bu.edu>
2023-11-01 18:59:10 -0400

    styled log in page

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Typography, Form } from "antd";
 
 const Login = () => {
-  return <div>Login</div>;
+  return <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      backgroundColor: "#eaf7f0",
+    }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "30%",
+        height: "51%",
+        backgroundColor: "#ffffff",
+        borderRadius: "8px",
+        boxShadow: "0px 0px 5px 0px rgba(0,0,0,0.2)",
+      }}>
+      <Typography.Title level={2}>Log in</Typography.Title>
+    </div>
+  </div>;
 };
 
 export default Login;
