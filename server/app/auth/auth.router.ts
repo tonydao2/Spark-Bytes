cd1dd27bf223c89cca938fc00f099a355cf965d0
mehadh <zlotytozli@gmail.com>
2023-11-01 19:43:30 -0400

    auth router ts

--- server/app/auth/auth.router.ts
+++ server/app/auth/auth.router.ts
@@ -1,2 +1,7 @@
-// Look at the other router files and figure out how to implement the auth router
-// Until you do, you will not be able to login or register or even make requests to those routes
+import express from 'express';
+import { authToken } from '../middleware/authToken.ts';
+import * as authController from './auth.controller.ts';
+
+const router = express.Router();
+router.use(authToken); // do we need this 
+router.post('/signup', authController.signup); // went to the route
