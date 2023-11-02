499a5389eda87f3e0cff60c7c32d179d1e943232
carolriady <carolriady@gmail.com>
2023-11-01 20:33:38 -0400

    worked on login page

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-import { Typography, Form } from "antd";
+import { Typography, Card, Input, Button, Space } from "antd";
+import type { FormItemProps } from 'antd';
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 
-const Login = () => {
+
+
+const Login: React.FC = () => {
+
+  const [passwordVisible, setPasswordVisible] = React.useState(false);
   return <div
     style={{
       display: "flex",
@@ -11,7 +17,7 @@ const Login = () => {
       height: "100vh",
       backgroundColor: "#eaf7f0",
     }}>
-    <div
+    {/* <div
       style={{
         display: "flex",
         flexDirection: "column",
@@ -22,10 +28,37 @@ const Login = () => {
         backgroundColor: "#ffffff",
         borderRadius: "8px",
         boxShadow: "0px 0px 5px 0px rgba(0,0,0,0.2)",
-      }}>
-      <Typography.Title level={2}>Log in</Typography.Title>
-    </div>
-  </div>;
+      }}> */}
+    <Card
+      style={{
+        width: "30%",
+        marginTop: "16px",
+        marginBottom: "16px",
+        boxShadow: "rgba(0, 0, 0, 0.15)  0px 2px 8px"
+      }}
+      bordered>
+      <div>
+        <Typography.Title level={2}
+          style={{
+            textAlign: "center",
+            color: "rgb(69, 90, 100)",
+          }}>Log In</Typography.Title>
+        <div>
+          <Typography.Text>Email Address</Typography.Text>
+          <Input
+            placeholder="Email"
+
+          />
+          <Typography.Text>Password</Typography.Text>
+          <Input.Password
+            placeholder="Password"
+            iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+          />
+        </div>
+      </div>
+    </Card>
+  </div >
+
 };
 
 export default Login;
